98262a239cf91b01a8fc86e54c9df4fee52ba85f
Surya Andhika Putri <161789260+ayandhikaa@users.noreply.github.com>
2025-02-05 20:05:48 +0700

    Update index.blade.php

--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -1,8 +1,4 @@
 <div>
-   @foreach($posts as $post)
-   {{ $post->description }}
-
-
 
 <div class="relative overflow-x-auto">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -14,57 +10,19 @@
                 <th scope="col" class="px-6 py-3">
                     Description
                 </th>
-                <th scope="col" class="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                </th>
             </tr>
         </thead>
         <tbody>
+            @foreach($posts as $post)
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {{ $post->title }}
                 </th>
                 <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
+                    {{ $post->description }}
                 </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                    Black
-                </td>
-                <td class="px-6 py-4">
-                    Accessories
-                </td>
-                <td class="px-6 py-4">
-                    $99
-                </td>
-            </tr>
+            {{ $post->description }}
         </tbody>
     </table>
     {{ $posts->links() }}
